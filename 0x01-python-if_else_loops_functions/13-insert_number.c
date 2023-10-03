940f65b954358a0e50689573c16ea7553de7d65a
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-03 09:35:16 -0700

    Fixed compilation errors

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,7 +10,7 @@
 */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *temp = head, *node;
+	listint_t *temp = *head, *node;
 
 	node = malloc(sizeof(listint_t));
 	if (!node)
@@ -26,4 +26,6 @@ listint_t *insert_node(listint_t **head, int number)
 
 		temp = temp->next;
 	}
+
+	return (temp);
 }
