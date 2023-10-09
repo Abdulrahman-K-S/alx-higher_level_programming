ae95c2e75821d2cc28ba0f684f0bbebb4e80cb50
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-09 11:07:11 -0700

    Fixed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,83 +1,40 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
-  * is_palindrome - Checks if a singly linked list is a palindrome
-  * @head: The head of the singly linked list
-  *
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-  */
+ * is_palindrome - that checks if a singly linked list is a palindrome.
+ *
+ * @head: pointer to pointer
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
 int is_palindrome(listint_t **head)
 {
-	listint_t *start = NULL, *end = NULL;
-	unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+	int list[1000000], i = 0, j = 0, k = 0;
+	listint_t *temp;
 
-	if (head == NULL)
-		return (0);
-
-	if (*head == NULL)
+	if (head == NULL || (*head) == NULL)
 		return (1);
-
-	start = *head;
-	len = listint_len(start);
-	len_cyc = len * 2;
-	len_list = len_cyc - 2;
-	end = *head;
-
-	for (; i < len_cyc; i = i + 2)
+	temp = *head;
+	while (temp)
 	{
-		if (start[i].n != end[len_list].n)
+		list[i] = temp->n;
+		i++;
+		temp = temp->next;
+	}
+	i--;
+
+	if (i % 2 != 0)
+		k = (i + 1) / 2;
+	else
+		k = i / 2;
+
+	while (j < k)
+	{
+		if (list[j] != list[i])
 			return (0);
-
-		len_list = len_list - 2;
+		i--;
+		j++;
 	}
-
 	return (1);
-}
-
-/**
-  * get_nodeint_at_index - Gets a node from a linked list
-  * @head: The head of the linked list
-  * @index: The index to find in the linked list
-  *
-  * Return: The specific node of the linked list
-  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	listint_t *current = head;
-	unsigned int iter_times = 0;
-
-	if (head)
-	{
-		while (current != NULL)
-		{
-			if (iter_times == index)
-				return (current);
-
-			current = current->next;
-			++iter_times;
-		}
-	}
-
-	return (NULL);
-}
-
-/**
-  * listint_len - Counts the number of elements in a linked list
-  * @h: The linked list to count
-  *
-  * Return: Number of elements in the linked list
-  */
-size_t listint_len(const listint_t *h)
-{
-	int lenght = 0;
-
-	while (h != NULL)
-	{
-		++lenght;
-		h = h->next;
-	}
-
-	return (lenght);
 }
