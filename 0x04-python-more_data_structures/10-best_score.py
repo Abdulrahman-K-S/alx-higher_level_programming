cf352b0df2f77cf924609bade04d7614a96f50aa
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-11 11:35:29 -0700

    Added '10-best_score.py''

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+
+    if a_dictionary == None:
+        return None
+
+    score = 0
+    for k, v in a_dictionary.items():
+        if score < v:
+            score = v
+            score_key = k
+
+    return score_key
