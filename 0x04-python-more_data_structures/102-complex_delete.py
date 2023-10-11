5c682c50b32e5f8e4d6bd302b56044a0a1cb0ef7
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-11 12:16:50 -0700

    Fixed

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
+    keys = []
+
     for k, v in a_dictionary.items():
         if v == value:
-            del a_dictionary[k]
-            break
+            keys.append(k)
+
+    for k in keys:
+        del a_dictionary[k]
     return a_dictionary
