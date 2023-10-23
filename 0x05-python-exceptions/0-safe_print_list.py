cc9e4767cfe380f82374efd8e4298d735b5857ff
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-23 12:53:36 -0700

    Updated '0-safe_print_list.py'
    Since there's no knowing what type is the list therefore i changed
    the format from {:d} to {}

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if idx < x:
-                print("{:d}".format(my_list[idx]), end='')
+                print("{}".format(my_list[idx]), end='')
                 idx += 1
     except TypeError:
         pass
