958d98700e7e2e722831d416d48b353781bf5f9a
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-23 13:15:13 -0700

    Added '2-safe_print_list_integers.py'

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idx = 0
+
+    for i in range(x):
+        try:
+            if type(my_list[i]) is int and idx != x:
+                print("{:d}".format(my_list[i]), end='')
+                idx += 1
+        except TypeError:
+            continue
+
+    print()
+    return idx
