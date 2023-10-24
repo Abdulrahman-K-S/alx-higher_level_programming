b6f082053b998460eb38142cd85e47f665a6426d
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-24 12:39:53 -0700

    Fixed style #3

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""Square class
+
+At the moment this class doesn't do anything
+
+"""
 class Sqaure:
-    """Square class
-
-    At the moment this class doesn't do anything
-
-    """
     pass
