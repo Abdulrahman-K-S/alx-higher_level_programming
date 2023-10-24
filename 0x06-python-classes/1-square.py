f73763e2a358610de8335d088e1aee06f705460c
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-24 12:43:54 -0700

    Added '1-square.py'

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square class
+
+A square class
+
+"""
+class Sqaure:
+    
+    def __init__(self, size):
+        """__init__
+
+        The __init__ methon initalizes the size value
+        of the square.
+
+        Attributes:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
