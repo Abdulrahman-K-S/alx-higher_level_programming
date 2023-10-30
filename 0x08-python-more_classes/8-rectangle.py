cc51c518976a18610d290de01f82a5042e012f1d
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-10-30 12:47:56 -0700

    Fixed

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -188,7 +188,7 @@ class Rectangle:
         if type(rect_1) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        elif rect_1.area() < rect_2.area():
+        else:
             return rect_2
