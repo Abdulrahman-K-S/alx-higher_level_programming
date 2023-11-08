43de604d3963d42c24805d312d6d686b97db824b
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-08 11:04:30 -0800

    Fixed '0-read_file.py'

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''A module that reads the inputed file'''
+
+
 def read_file(filename=""):
     ''' function that reads a text file (UTF8) and prints it to stdout '''
     with open(filename, encoding='utf-8') as f:
