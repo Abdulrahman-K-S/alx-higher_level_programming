3735aac9d6b16d6f0c0c4085aeab3484b6b6c702
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-08 10:49:41 -0800

    Fixed '10-square.py'

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+'''
+Rectangle module inhearted to the Square class
+'''
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    '''class Square
+
+    Attributes:
+        __size (int): The size of the square.
+    '''
     def __init__(self, size):
+        '''The Square class constructor'''
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
