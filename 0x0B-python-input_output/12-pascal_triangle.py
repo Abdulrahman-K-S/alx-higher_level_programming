d8de9cac3cda8983111f4437f95511ced0fa3dce
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-08 11:41:21 -0800

    Added '12-pascal_triangle.py'

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+Pascal Triangle
+'''
+
+
+def pascal_triangle(n):
+    '''pascal_triangle
+    '''
+    if n <= 0:
+        return []
+
+    limit = n - 1
+    triangle = [[1]]
+
+    for i in range(limit):
+        row = []
+        row.append(1)
+
+        if len(triangle[i]) > 1:
+            prev_row_len = len(triangle[i]) - 1
+            nxt = 1
+
+            for j in range(prev_row_len):
+                suma = triangle[i][j] + triangle[i][nxt]
+                row.append(suma)
+                nxt += 1
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
