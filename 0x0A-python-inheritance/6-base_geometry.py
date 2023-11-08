eb7903b88204353a233bc7613054bce8a189cb02
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-08 10:41:25 -0800

    Fixed '6-base_geometry.py'

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+'''
+BaseGeometry module
+'''
+
+
 class BaseGeometry:
+    '''class BaseGeometry'''
     def area(self):
+        '''Public class area
+
+        Raises an exception
+
+        Raises:
+              Exception
+        '''
         raise Exception('area() is not implemented')
