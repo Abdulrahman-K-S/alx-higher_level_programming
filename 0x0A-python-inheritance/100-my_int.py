fc59f2445fde5dde9e85860c7ce54ba3f3268b80
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-08 10:54:35 -0800

    Added '100-my_int.py'

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' class MyInt that inherits from int '''
+
+
+class MyInt(int):
+    ''' MyInt is a rebel. MyInt has == and
+        != operators inverted
+    '''
+    def __init__(self, number):
+        '''The MyInt class constructor'''
+            self.number = number
+
+    def __eq__(self, other):
+        '''__eq__
+
+        The equal operator to be inverted.
+        '''
+        if self.number == int(other):
+            return False
+        return True
+
+    def __ne__(self, other):
+        '''__ne__
+
+        The not-equal operator to be inverted.
+        '''
+        if self.number != int(other):
+            return False
+        return True
