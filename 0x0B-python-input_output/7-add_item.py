a3e318d2b385081ba792f67750d60fc34de48523
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-08 11:24:25 -0800

    Fixed num typo

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ and then save them to a file
 
 from os import path
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if path.exists('add_item.json'):
     obj_json_file = load_from_json_file('add_item.json')
