905b5243e9faeabb45a81d442efb066243b0300b
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-11-11 12:54:25 -0800

    Updated 'square.py'
    Added public size getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,16 @@ class Square(Rectangle):
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width
         ))
+
+    @property
+    def size(self):
+        """size
+
+        Getter for the size attribute (aka width/height).
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
