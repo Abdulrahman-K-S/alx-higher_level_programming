019fca6b2ece8b4b6367956a4e28286809aef1f5
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-12 13:00:46 -0800

    Added '7-insert_value.sql'
    A script that inserts a new row into the table 'first_table' from the database 'hbtn_0c_0'.
    The values are:
    - id = 89
    - name =
    'Best School'.

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- A script that inserts a new row into the table `first_table` from the
+-- database `hbtn_0c_0`.
+-- 	    Attributes added:
+-- 	    	       - id = 89
+-- 		       - name = 'Best School'
+INSERT INTO first_table (id, `name`) VALUES (89, 'Best School');
