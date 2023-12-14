00210c816570a5ddb2c2a893fb7779839b62b11d
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-13 16:48:29 -0800

    Updated '1-create_user.sql'
    Fixed the type of uppercase

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- A script that creats the user `user_0d_1`
-DROP USER if exists 'user_0d_1'@localhost;
 CREATE USER if not exists 'user_0d_1'@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'USER_0D_1'@localhost;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@localhost;
 FLUSH PRIVILEGES;
