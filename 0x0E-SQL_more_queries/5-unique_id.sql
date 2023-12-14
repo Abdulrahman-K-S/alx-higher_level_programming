b130067c24fd902bef467e0cfb3ae134e6efadab
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-13 17:05:42 -0800

    Added '5-unique_id.sql'
    A script that creates the table 'unique_id' on the given database.
    If table already exists the script doesn't fail.
    Table description:
     - id INT DEFAULT 1 UNIQUE
     - name VARCHAR(256)

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,8 @@
+-- A script that creates the table `unique_id` on the given database.
+-- Table description:
+-- 	 - id INT default 1 unique
+-- 	 - name VARCHAR(256)
+CREATE TABLE if not exists unique_id(
+       id INT DEFAULT 1 UNIQUE,
+       'name' VARCHAR(256)
+);
