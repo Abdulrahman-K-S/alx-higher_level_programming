e3c875da125042d679de185daa3e0648f6e2ce2d
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-13 16:27:39 -0800

    Updated '1-create_user.sql'
    Added the drop user if the user exists

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- A script that creats the user `user_0d_1`
+DROP USER if exists 'user_0d_1'@'localhost';
 CREATE USER if not exists 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'USER_0D_1'@'localhost';
 FLUSH PRIVILEGES;
