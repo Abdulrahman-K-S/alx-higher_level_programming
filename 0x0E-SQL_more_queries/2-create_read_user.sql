552e29fceb3e79d6a6d65af0ee628785b88201e0
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-13 16:42:52 -0800

    Added '2-create_read_user.sql'
    A script that creates the database 'hbtn_0d_2'
    and the user 'user_0d_2' with SELECT privileges.
    If either exists the script shouldn't fail.

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- A script that creates the database `hbtn_0d_2`
+-- and the user `user_0d_2` with SELECT privileges only
+CREATE DATABASE if not exists hbtn_0d_2;
+CREATE USER if not exists 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT on *.* TO 'user_0d_2'@localhost;
+FLUSH PRIVILEGES;
