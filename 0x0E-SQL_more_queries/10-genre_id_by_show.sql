27158ddd1db03a6e99c8b8aacc428f76c3fda69e
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-13 17:59:12 -0800

    Added '10-genre_id_by_show.sql'
    A script that lists all shows contained in  that have at least one genre linked.
    Can use SELECT only once.

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,11 @@
+-- A script that lists all shows contained in `hbtn_0d_tvshows` that have at least one genre linked.
+SELECT
+	shows.title,
+	genres.genre_id
+FROM
+	tv_shows AS shows,
+	tv_show_genres AS genres
+WHERE
+	shows.id = genres.genre_id
+ORDER BY
+      shows.title, genres.genre_id ASC
