a7aaf02c2fe5caa3b2779ea432ce6b43d3022781
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-13 16:46:09 -0800

    Updated '2-create_read_user.sql'
    Fixed runtime error

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- and the user `user_0d_2` with SELECT privileges only
 CREATE DATABASE if not exists hbtn_0d_2;
 CREATE USER if not exists 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on hbtn_0d_2 TO 'user_0d_2'@localhost;
+GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@localhost;
 FLUSH PRIVILEGES;
