f59aa188e6e057c9a9d4781dcf6add1b5b80dde8
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-13 17:48:54 -0800

    Added '9-cities_by_state_join.sql'
    A script that lists all the cities contained in the database 'hbtn_0d_usa'.
    Can only use one SELECT statment.

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,12 @@
+-- A script that lists all cities contained in the database `hbtn_0d_usa`.
+SELECT
+	c.id,
+	c.name,
+	s.name
+FROM
+	cities AS c,
+	states AS s
+WHERE
+	s.id = c.state_id
+ORDER BY
+      c.id ASC
