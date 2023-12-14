70a61519a622238f2dd7b722f4dad459932b7ad3
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-13 17:40:48 -0800

    Added '8-cities_of_california_subquery.sql'
    A script that lists all the cities of California that can be found in the database 'hbtn_0d_usa'.
    Can't use JOIN

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,12 @@
+-- A script that lists all the cities of California that can be found in the database `hbtn_0d_usa`.
+-- Can't use JOIN.
+SELECT
+	id,
+	name
+FROM
+	states AS s,
+	citites AS c
+WHERE
+	s.id = c.id
+ORDER BY
+      c.id DESC
